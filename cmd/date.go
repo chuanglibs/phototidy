91de094780090fde4cd43d3c395baaed848e00a8
Todd <chan_jam@qq.com>
2025-11-11 23:32:21 +0800

    基础实现读取照片和视频的时间

--- cmd/date.go
+++ cmd/date.go
@@ -6,9 +6,17 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 
+	"github.com/abema/go-mp4"
+	"github.com/rwcarlsen/goexif/exif"
 	"github.com/spf13/cobra"
 )
+
+var dir string
 
 // dateCmd represents the date command
 var dateCmd = &cobra.Command{
@@ -18,20 +26,209 @@ var dateCmd = &cobra.Command{
 以其拍摄日期为准来进行目录分类，按年份-》月份来进行目录创建，并把图片、视频文件移动到对应的目录中
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("date called")
+		if err := processPhotos(); err != nil {
+			fmt.Printf("处理照片时出错: %v\n", err)
+			os.Exit(1)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(dateCmd)
+	dateCmd.Flags().StringVarP(&dir, "dir", "d", ".", "指定要处理的目录")
+}
 
-	// Here you will define your flags and configuration settings.
+// processPhotos 处理目录中的照片
+func processPhotos() error {
+	// 获取绝对路径
+	absDir, err := filepath.Abs(dir)
+	if err != nil {
+		return fmt.Errorf("获取目录绝对路径失败: %v", err)
+	}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// dateCmd.PersistentFlags().String("foo", "", "A help for foo")
+	fmt.Printf("开始处理目录: %s\n", absDir)
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// dateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// 支持的图片格式
+	supportedFormats := []string{".jpg", ".jpeg", ".png", ".tiff", ".tif", ".heic", ".mp4", ".mov", ".avi"}
+
+	// 计数器
+	totalFiles := 0
+	exifFiles := 0
+	videoFiles := 0
+	modTimeFiles := 0
+
+	// 遍历目录
+	err = filepath.Walk(absDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// 跳过目录
+		if info.IsDir() {
+			return nil
+		}
+
+		// 检查文件扩展名
+		ext := strings.ToLower(filepath.Ext(path))
+		isSupported := false
+		for _, format := range supportedFormats {
+			if ext == format {
+				isSupported = true
+				break
+			}
+		}
+
+		if !isSupported {
+			return nil
+		}
+
+		totalFiles++
+
+		// 获取照片时间信息
+		photoTime, timeSource, err := getPhotoTimeInfo(path)
+		if err != nil {
+			fmt.Printf("警告: 无法获取 %s 的时间信息: %v\n", path, err)
+			return nil
+		}
+
+		// 根据时间来源进行区分显示
+		switch timeSource {
+		case "EXIF":
+			exifFiles++
+			fmt.Printf("[EXIF] %s: %s\n", path, photoTime.Format("2006-01-02 15:04:05"))
+		case "视频元数据":
+			videoFiles++
+			fmt.Printf("[视频元数据] %s: %s\n", path, photoTime.Format("2006-01-02 15:04:05"))
+		default:
+			modTimeFiles++
+			fmt.Printf("[文件时间] %s: %s\n", path, photoTime.Format("2006-01-02 15:04:05"))
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return fmt.Errorf("遍历目录失败: %v", err)
+	}
+
+	fmt.Printf("\n处理完成！\n")
+	fmt.Printf("总计找到 %d 个照片/视频文件\n", totalFiles)
+	fmt.Printf("从EXIF获取时间: %d 个\n", exifFiles)
+	fmt.Printf("从视频元数据获取时间: %d 个\n", videoFiles)
+	fmt.Printf("从文件时间获取: %d 个\n", modTimeFiles)
+
+	return nil
+}
+
+// getPhotoTimeInfo 获取照片时间信息和来源
+func getPhotoTimeInfo(filePath string) (time.Time, string, error) {
+	ext := strings.ToLower(filepath.Ext(filePath))
+
+	// 根据文件类型选择不同的处理方式
+	if isVideoFile(ext) {
+		// 视频文件：尝试从MP4元数据获取创建时间
+		videoTime, err := getVideoCreationTime(filePath)
+		if err == nil && !videoTime.IsZero() {
+			return videoTime, "视频元数据", nil
+		}
+	} else {
+		// 图片文件：首先尝试从EXIF数据获取拍摄日期
+		exifTime, err := getExifDateTime(filePath)
+		if err == nil && !exifTime.IsZero() {
+			return exifTime, "EXIF", nil
+		}
+	}
+
+	// 如果专用元数据不可用，使用文件修改时间
+	info, err := os.Stat(filePath)
+	if err != nil {
+		return time.Time{}, "", fmt.Errorf("无法获取文件信息: %v", err)
+	}
+
+	return info.ModTime(), "文件时间", nil
+}
+
+// isVideoFile 判断是否为视频文件
+func isVideoFile(ext string) bool {
+	videoExts := []string{".mp4", ".mov", ".avi", ".mkv", ".flv", ".wmv"}
+	for _, videoExt := range videoExts {
+		if ext == videoExt {
+			return true
+		}
+	}
+	return false
+}
+
+// getExifDateTime 从EXIF数据获取拍摄日期时间
+func getExifDateTime(filePath string) (time.Time, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return time.Time{}, err
+	}
+	defer file.Close()
+
+	// 解码EXIF数据
+	x, err := exif.Decode(file)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	// 获取拍摄日期
+	date, err := x.DateTime()
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return date, nil
+}
+
+// getVideoCreationTime 从视频元数据获取创建时间
+func getVideoCreationTime(filePath string) (time.Time, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return time.Time{}, err
+	}
+	defer file.Close()
+
+	// 使用go-mp4库读取视频元数据
+	var creationTime time.Time
+
+	// 创建MP4读取器
+	_, err = mp4.ReadBoxStructure(file, func(h *mp4.ReadHandle) (interface{}, error) {
+		switch h.BoxInfo.Type.String() {
+		case "mvhd":
+			// Movie header box
+			box, _, err := h.ReadPayload()
+			if err != nil {
+				return nil, err
+			}
+			if mvhd, ok := box.(*mp4.Mvhd); ok {
+				creationTimeInt64 := mvhd.GetCreationTime()
+				creationTime = time.Unix(int64(creationTimeInt64), 0)
+			}
+		case "mdhd":
+			// Media header box
+			box, _, err := h.ReadPayload()
+			if err != nil {
+				return nil, err
+			}
+			if mdhd, ok := box.(*mp4.Mdhd); ok {
+				if creationTime.IsZero() {
+					creationTimeInt64 := mdhd.GetCreationTime()
+					creationTime = time.Unix(int64(creationTimeInt64), 0)
+				}
+			}
+		}
+		return nil, nil
+	})
+
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	if creationTime.IsZero() {
+		return time.Time{}, fmt.Errorf("未找到视频创建时间")
+	}
+
+	return creationTime, nil
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.24.4
 require github.com/spf13/cobra v1.10.1
 
 require (
+	github.com/abema/go-mp4 v1.4.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
 )
